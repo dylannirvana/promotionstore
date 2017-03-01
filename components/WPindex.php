b201a25313ecaa3395368395187d6e4b6df64548
Dylan Nirvana <dylan@dylannirvana.com>
2017-03-01 15:49:46 -0500

    reworking Bootstrap prototype

---
+++ components/WPindex.php
@@ -0,0 +1,204 @@
+<?php get_header(); ?>
+
+
+
+      <!-- MAIN SECTION -->
+      <div class="row">
+
+        <!-- ARTICLE -->
+        <article class="col-md-8">
+          <!-- main introduction -->
+          <h2>Main introduction</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
+          <div class="row">
+            <div class="card col-md-12">
+              <img class="card-img-top" src="http://placehold.it/600x280" alt="" >
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">More</a> </p>
+              <a href="#" class="btn btn-primary">Buy now or Rent!</a>
+            </div>
+          </div>
+
+          <div>
+            <h4>Secondary article</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+          <div class="row">
+            <div class="card col-sm-6">
+              <img class="card-img-top" src="http://placehold.it/280x140" alt="">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">More</a> </p>
+              <a href="#" class="btn btn-primary">Buy now or Rent!</a>
+            </div>
+            <div class="card col-sm-6">
+              <img class="card-img-top" src="http://placehold.it/280x140" alt="">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <a href="#">More</a> </p>
+              <a href="#" class="btn btn-primary">Buy now or Rent!</a>
+            </div>
+          </div>
+
+          <div>
+            <h4>Secondary article</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div>
+            <h4>Secondary article</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+        </article>
+
+        <!-- ASIDE / SIDEBAR -->
+        <aside class="col-md-4">
+          <h2>Sidebar</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
+
+          <!-- ROWS -->
+          <div class="row">
+
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+            <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+              <a href="#">
+                <h6 class="card-title">Item title </h6>
+              </a>
+            </div>
+
+          </div> <!-- end row -->
+
+            <div class="row sidebar-text">
+              <div class="col-md-12">
+                <h4>Break it up</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+              <div class="card col-sm-6 col-md-4 col-lg-3">
+                <img class="card-img-top" src="http://placehold.it/60x80" alt="" >
+                <a href="#">
+                  <h6 class="card-title">Item title </h6>
+                </a>
+              </div>
+
+            </div> <!-- end row -->
+
+
+        </aside>
+
+      </div> <!-- end content row -->
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2017</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+  <?php get_footer(); ?>
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
     concat = require('gulp-concat');
 
 var env,
@@ -14,12 +16,18 @@ var env,
     sassSources,
     htmlSources,
     outputDir,
+    wpDir,
     sassStyle;
+
+    /* THIS IS THE WORDPRESS DIRECTORY */
+    wpDir = 'wordpress/wp-content/themes/promotionstoretheme/';
 
 // ENVIRONMENT
 env = process.env.NODE_ENV || 'development';
+
 if (env==='development') {
-  outputDir = 'builds/development/';
+  // outputDir = wpDir; // to WordPress
+  outputDir = 'builds/development/';  // to bootstrap
   sassStyle = 'expanded';
 } else {
   outputDir = 'builds/production/';
@@ -29,19 +37,23 @@ if (env==='development') {
 // PATHS
 bowerSources = ['bower_components/**/'];
 jsSources = [
-  'components/scripts/javascript.js',
   bowerSources + 'dist/jquery.min.js',
-  bowerSources + 'dist/js/*.min.js'
+  bowerSources + 'dist/js/*.min.js',
+  // 'bower_components/jquery/dist/jquery.min.js',
+  // 'bower_components/tether/dist/js/tether.min.js',
+  // 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+  'components/scripts/javascript.js'
  ];
 
 sassSources = [ 'components/sass/style.scss' ];
-htmlSources = [outputDir + '*.html'];
+htmlSources = [ 'components/*.html'];
 
 // TASKS
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .on('error', gutil.log)
     .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
@@ -49,13 +61,17 @@ gulp.task('js', function() {
 
 gulp.task('compass', function() {
   gulp.src(sassSources)
+    .pipe(sourcemaps.init())
     .pipe(compass({
       sass: 'components/sass',
       image: outputDir + 'images',
+      css: outputDir + 'css',
+      sourcemap: true,
       style: sassStyle
     })
     .on('error', gutil.log))
-    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(sourcemaps.write(outputDir))
+    .pipe(gulp.dest(outputDir))
     .pipe(connect.reload())
 });
 
@@ -63,7 +79,7 @@ gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('components/sass/**./*.scss', ['compass']);
-  gulp.watch('builds/development/*.html', ['html']);
+  gulp.watch('components/*.html', ['html']);
   // gulp.watch('builds/development/js/*.json', ['json']);
   gulp.watch('builds/development/images/**/*.*', ['images']);
 });
@@ -77,8 +93,11 @@ gulp.task('connect', function() {
 
 gulp.task('html', function() {
   gulp.src('builds/development/*.html')
+  // gulp.src('components/*.html')
     .pipe(gulpif(env === 'production', minifyHTML()))
     .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    // .pipe(rename('front-page.php'))
+    .pipe(gulp.dest(outputDir))
     .pipe(connect.reload())
 });
 
